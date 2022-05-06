2669291da7b5b8b49b0260fb44c240584227497b
Anderleg <105012832+Anderleg@users.noreply.github.com>
2022-05-06 15:08:21 -0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 15:03:21 2022
+
+@author: ANDERSON
+"""
+
+variable=500
+palabra="Cisco"
+exprecion=palabra*variable
+print("\n*2")
+print("otra linea")
+print("\n*3")
+print("Hola Mundo")
