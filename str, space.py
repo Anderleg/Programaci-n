f3c6bdc43c818467210454e81cfc537a7e67b85f
Anderleg <105012832+Anderleg@users.noreply.github.com>
2022-05-06 15:15:06 -0500

    Add files via upload

---
+++ str, space.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 15:07:59 2022
+
+@author: ANDERSON
+"""
+
+str1="Cisco"
+str2="Networking"
+str3="Academy"
+space=" "
+print=(str1+space+str2+space+str3)
+print=("\n"*2)
+print=(str1+str2+str3)
+print=("\n"*3)
+print=(str1+str2+str3)
+
+      
