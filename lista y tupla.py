89ce6c962483a2c85c3cf115d84226808e02e031
Anderleg <105012832+Anderleg@users.noreply.github.com>
2022-05-06 15:38:10 -0500

    Add files via upload

---
+++ lista y tupla.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 15:24:22 2022
+
+@author: ANDERSON
+"""
+
+lista1=[5,58,"cisco","Networking",
+       True,False,12,8,85,5]
+tupla1=(5,58,"cisco","Networking",
+       True,False,12,8,85,5)
+print(lista1)
+print("\n"*2)
+print(tupla1)
+print("\n"*2)
+print(lista1[0])
+print("\n"*2)
+print(lista1[8])
+print(tupla1[0])
+print(tupla1[8])
+lista1[7]="Academy"
+tupla1[7]="Academy"
+len(lista1)
+del lista1[7]
+print(lista1[-8])
+
