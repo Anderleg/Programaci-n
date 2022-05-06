60da1e641a9a6305303a8816e78f98f7f69c1189
Anderleg <105012832+Anderleg@users.noreply.github.com>
2022-05-06 15:21:56 -0500

    Add files via upload

---
+++ str,int,boll,float cambios.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 15:15:30 2022
+
+@author: ANDERSON
+"""
+
+a=int()
+b=str()
+c=float()
+d=bool()
+x=5
+print("El valor de x es:"+str(x))
+x=str(x)
+print(type(x))
+x=float(x)
+print(type(x))
+x=int(x)
+print(type(x))
+x=bool(x)
+print(type(x))
