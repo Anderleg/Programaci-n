65bdc1aa789b3ed5f7b902d70045f4b6f347c739
Anderleg <105012832+Anderleg@users.noreply.github.com>
2022-05-06 15:54:20 -0500

    Add files via upload

---
+++ country and capitals.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 15:38:43 2022
+
+@author: ANDERSON
+"""
+
+country =[
+        "brazil"
+        "russia"
+        "india"
+        "china"
+        "south Africa"]
+capitals = {
+        "brazil":"brazilian",
+        "russia":"Moscú",
+        "india":"nueva delhi",
+        "china":"Beijin",
+        "south Africa":[
+                           "pretoria",
+                           "cape town",
+                           "bloemfontein"
+                           ]
+           }
+print(country)
+print(capitals)
+"""
+What response did you get?
+Why did the list and dictionary
+  contents not print?
+Fix the code and run the scrip again
+"""
+
+print(capitals["south Africa"][1])
+"""
+Why did get an error for the 
+  2nd capital of South Africa?
+Hint; Check the syntax for the index value.
+"""  
