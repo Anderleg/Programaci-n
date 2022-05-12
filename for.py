aec256d5e70d4f82e693dab5585e6df92e7eb205
Anderson Calo <105012832+Anderleg@users.noreply.github.com>
2022-05-12 10:10:55 -0500

    Add files via upload

---
+++ for.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 10:07:10 2022
+
+@author: ANDERSON
+"""
+
+lista=["R2",
+       "R2",
+       "R3",
+       "S1",
+       "S2",
+       "S3",]
+
+for item in lista:
+    print(item,end=" ")
+    
