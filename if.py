748586a6fb94fded199ea12c6e78b71d05ba22ec
Anderson Calo <105012832+Anderleg@users.noreply.github.com>
2022-05-12 09:57:08 -0500

    Add files via upload

---
+++ if.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 09:49:29 2022
+
+@author: ANDERSON
+"""
+
+datos=100
+nativa=1
+if datos==nativa:
+    print("Las VLAN son iguales")
+else:
+    if True:
+        print("Las VLAN son iguales")
+        if True:
+            print("Las VLAN son iguales")
+            
