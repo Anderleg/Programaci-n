4b5aef0278c4a4e1052ed08eda495e90c47cd0d3
Anderson Calo <105012832+Anderleg@users.noreply.github.com>
2022-05-12 10:30:20 -0500

    Add files via upload

---
+++ while.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 10:22:16 2022
+
+@author: ANDERSON
+"""
+
+numero=int(input("Ingrese el # a contar"))
+contador=1
+while contador <=numero:
+    print(contador)
+    contador+=1
+    
