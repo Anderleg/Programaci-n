52c5c0531e316250517b3ca90e81df6f4ab834ea
Anderson Calo <105012832+Anderleg@users.noreply.github.com>
2022-05-12 09:50:10 -0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 09:41:16 2022
+
+@author: ANDERSON
+"""
+
+nombre=input("Ingrese su numero: ")
+apellido=input("Ingrese su apellido: ")
+ubicacion=input("Ingrese su ubicacion de referencia: ")
+edad=input("Ingrse su edad: ")
+
+print("Su nombre es: ")+nombre+space+apellido+space("Su ubicacion es :")+ubicacion
+     +space("y su edad es :")+edad
+     
